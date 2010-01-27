c066481abc281fc0fd384ec4fc32830b98ec8b84
dcriori <dcriori@58a0b488-0aed-11df-89ef-dd47c58a1d1c>
2010-01-27 08:13:47 +0000

    mark

--- trunk/nexus-enterprise/src/main/java/it/nexus/enterprise/system/authorize/controller/RoleController.java
+++ trunk/nexus-enterprise/src/main/java/it/nexus/enterprise/system/authorize/controller/RoleController.java
@@ -171,8 +171,7 @@ public class RoleController extends BaseAction<Role> {
 			}
 		}
 	}
-
-	// TODO 瑕佸垱寤轰竴涓嚱鏁帮紝鏉ュ垱寤轰竴涓紶缁檆heckboxlist鐨勬暟缁�
+	
 	@SuppressWarnings("unchecked")
 	private String[] createArrRole(long rolebits, Map<Long, String> roles) {
 		List<String> access_roles = new ArrayList<String>();
@@ -191,80 +190,6 @@ public class RoleController extends BaseAction<Role> {
 		access_roles.toArray(result);
 		return result;
 	}
-
-//	// 鍦ㄧ被涓幓鎵剧浉鍏崇殑鏉冮檺淇℃伅锛屼俊鎭敱annotation閰嶇疆
-//	private void createPluginInfo() {
-//		for (String str_package : settingClass.getAccessToScan()) {
-//			System.out.println("浣犻厤缃簡锛�" + str_package);
-//			List<Class<?>> classFiles = ClassUtils.getAccessGroup(str_package);
-//
-//			for (Class<?> clazz : classFiles) {
-//				action_class_list.add(clazz);
-//			}
-//		}
-//
-//		for (Class<?> clazz : action_class_list) {
-//			findAccessGroup(clazz);
-//		}
-//	
-//		ActionContext.getContext().getSession().put("access_group_map",
-//				access_group_map);
-//		ActionContext.getContext().getSession().put("plugin_info", plugin_info);
-//	}
-
-//	@SuppressWarnings("unused")
-//	private void findAccessGroup(Class<?> action_clazz) {
-//		AccessGroup aGroup = action_clazz.getAnnotation(AccessGroup.class);
-//		Map<Long, String> access_actions = createAccessAction(action_clazz);
-//		String plugin_name = PluginClassUtil
-//				.getPluginName(aGroup.pluginClass());
-//		String plugin_dispname = PluginClassUtil.getPluginDispName(aGroup
-//				.pluginClass());
-//		Map<String, Map<Long, String>> access_map = genAccessGroupMap(plugin_name);
-//		createPluginInfo(plugin_name, plugin_dispname);
-//		PluginClassUtil.getPluginDispName(aGroup.pluginClass());
-//
-//		access_map.put(aGroup.name(), access_actions);
-//	}
-//
-//	@SuppressWarnings("unused")
-//	private void createPluginInfo(final String plugin_name,
-//			final String plugin_dispname) {
-//		if (plugin_info == null)
-//			plugin_info = new HashMap<String, String>();
-//		if (!plugin_info.containsKey(plugin_name)) {
-//			plugin_info.put(plugin_name, plugin_dispname);
-//		}
-//	}
-//
-//	private Map<String, Map<Long, String>> genAccessGroupMap(String tree_name) {
-//		if (access_group_map == null)
-//			access_group_map = new TreeMap<String, Map<String, Map<Long, String>>>();
-//		if (!access_group_map.containsKey(tree_name))
-//			access_group_map.put(tree_name,
-//					new HashMap<String, Map<Long, String>>());
-//		return access_group_map.get(tree_name);
-//	}
-
-//	private Map<Long, String> createAccessAction(Class<?> action_clazz) {
-//		Map<Long, String> result = new HashMap<Long, String>();
-//		Method[] methods = action_clazz.getMethods();
-//		for (Method method : methods) {
-//			Access access = method.getAnnotation(Access.class);
-//			if (access != null) {
-//				ValuedEnum operation = (ValuedEnum) EnumUtil.getEnum(access
-//						.enumClass(), access.value());
-//				if (operation == null)
-//					throw new Error(String.format(
-//							"褰撳墠Action锛�%s涓殑鏂规硶锛�%s娉ㄩ噴鏈夎,鎵句笉鍒版潈闄�:[%s];", action_clazz
-//									.getSimpleName(), method.getName(), access
-//									.value()));
-//				String name = access.value().toString();
-//				result.put(new Long(operation.getValue()), name);
-//			}
-//		}
-//		return result;
-//	}
 
 	/*浠ヤ笅鏄痝eter seter灞炴��*/
 	private Role								dmo;
