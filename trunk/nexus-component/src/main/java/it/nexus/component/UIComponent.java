9a58b94edac1017b645918d602924eaf3d6a4fc2
dcriori <dcriori@58a0b488-0aed-11df-89ef-dd47c58a1d1c>
2010-02-21 14:34:19 +0000

    mark create nexus-component project

---
+++ trunk/nexus-component/src/main/java/it/nexus/component/UIComponent.java
@@ -0,0 +1,31 @@
+package it.nexus.component;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.LinkedList;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Administrator
+ * Date: 2010-2-21
+ * Time: 14:00:08
+ * To change this template use File | Settings | File Templates.
+ */
+public class UIComponent  {
+    protected List<UIComponent> controls;
+    public UIComponent add(UIComponent uiComponent){
+        if(controls==null)
+            controls=new LinkedList<UIComponent>();
+        controls.add(uiComponent);
+        return this;
+    }
+
+    public int size(){
+        if(controls!=null)
+            return controls.size();
+        return 0;
+    }
+
+    
+}
