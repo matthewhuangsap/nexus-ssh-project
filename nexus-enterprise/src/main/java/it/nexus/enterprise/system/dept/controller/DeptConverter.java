7b830955ee7787a10b706380ddccbc5a9900fa74
dcriori <dcriori@58a0b488-0aed-11df-89ef-dd47c58a1d1c>
2010-02-23 02:25:46 +0000

    mark

--- nexus-enterprise/src/main/java/it/nexus/enterprise/system/dept/controller/DeptConverter.java
+++ nexus-enterprise/src/main/java/it/nexus/enterprise/system/dept/controller/DeptConverter.java
@@ -2,31 +2,24 @@ package it.nexus.enterprise.system.dept.controller;
 
 import it.nexus.enterprise.system.dept.model.Dept;
 import it.nexus.enterprise.system.dept.service.DeptService;
-
 import java.util.Map;
-
 import javax.annotation.Resource;
-
 import org.apache.struts2.util.StrutsTypeConverter;
 
 @SuppressWarnings("unchecked")
-public class DeptConverter extends StrutsTypeConverter {
+public class DeptConverter extends StrutsTypeConverter  {
 	
 	@Resource
 	DeptService deptService;
-	@Override
+
+    @Override
 	public Object convertFromString(Map context, String[] values, Class toClass) {
-        System.out.println(">>>>>dept_id:"+values[0]);
-		String dept_id = values[0].toString();
-        System.out.println(">>>>>dept_id:"+dept_id);
+        String dept_id = values[0];
 		Dept dept = deptService.load(dept_id);
-		System.out.println("传入的值："+values[0]);
-		System.out.println("toClass："+toClass.getName());
-		
 		return dept;
 	}
 
-	@Override
+    @Override
 	public String convertToString(Map arg0, Object arg1) {
 		return null;
 	}
