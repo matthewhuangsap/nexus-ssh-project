426acf0a0c6a5f4df69cf503f2bd03b19f6531c5
dcriori <dcriori@58a0b488-0aed-11df-89ef-dd47c58a1d1c>
2010-02-23 09:35:03 +0000

    mark

--- nexus-enterprise/src/main/java/it/nexus/enterprise/system/dept/service/DeptService.java
+++ nexus-enterprise/src/main/java/it/nexus/enterprise/system/dept/service/DeptService.java
@@ -1,5 +1,6 @@
 package it.nexus.enterprise.system.dept.service;
 
+import it.nexus.core.service.BaseInfoService;
 import it.nexus.enterprise.system.dept.dao.DeptDAO;
 import it.nexus.enterprise.system.dept.model.Dept;
 
@@ -13,45 +14,11 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Transactional
 @Service
-public class DeptService {
+public class DeptService extends BaseInfoService<Dept,DeptDAO> {
 	
 	@Resource
-	DeptDAO deptDAO;
-	
-	public void start(Dept entity) {
-		deptDAO.start(entity);
-	}
-
-	public void stop(Dept entity) {
-		deptDAO.stop(entity);
-	}
-
-	public void lock(Dept entity) {
-		deptDAO.lock(entity);
-	}
-
-	public void unLock(Dept entity) {
-		deptDAO.unLock(entity);
-	}
-
-	public void delete(Dept entity) {
-		deptDAO.delete(entity);
-	}
-
-	public List<Dept> getAll() {
-		return deptDAO.getAll();
-	}
-
-	public Dept load(Serializable id) {
-		return deptDAO.load(id);
-	}
-
-	public void save(Dept entity) {
-		deptDAO.save(entity);
-	}
-
-	public void update(Dept entity) {
-		deptDAO.update(entity);
-	}
+	public void setDao(DeptDAO dao){
+        this.dao=dao;
+    }
 	
 }
