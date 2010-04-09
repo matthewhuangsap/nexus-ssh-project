a49cd8400aeaacc1704f9a4621601a4d338da214
dcriori <dcriori@58a0b488-0aed-11df-89ef-dd47c58a1d1c>
2010-04-09 09:22:41 +0000

    mark

--- nexus-core/src/main/java/it/nexus/core/web/PluginManager.java
+++ nexus-core/src/main/java/it/nexus/core/web/PluginManager.java
@@ -1,6 +1,5 @@
 package it.nexus.core.web;
 
-import com.opensymphony.xwork2.util.ClassLoaderUtil;
 import it.nexus.core.BasePlugin;
 import it.nexus.core.NexusException;
 import it.nexus.core.menu.Menu;
@@ -22,95 +21,115 @@ import java.util.jar.JarFile;
  * To change this template use File | Settings | File Templates.
  */
 public final class PluginManager {
-    private static Map<String, BasePlugin> plugins = new HashMap<String, BasePlugin>();
+  private static Map<String, BasePlugin> plugins = new HashMap<String, BasePlugin>();
 
-    public static void init(List<String> jar_list) {
-        for (String jar_path : jar_list) {
-            initPlugin(jar_path);
+  public static void init(List<String> jar_list) {
+    for (String jar_path : jar_list) {
+      initPlugin(jar_path);
+      initAccessInfo(jar_path);
+    }
+  }
+
+  private static void initAccessInfo(String jar_path) {
+    JarFile jarFile = null;
+    try {
+      jarFile = new JarFile(jar_path);
+      Enumeration<?> enu = jarFile.entries();
+      while (enu.hasMoreElements()) {
+        JarEntry entry = (JarEntry) enu.nextElement();
+        String name = entry.getName();
+        if (name.endsWith("Controller.class")||name.endsWith("Action.class")) {
+//          System.out.println("###########class name :" + name);
+          String className = name.replace(".class","").replace("/",".");
+          Class clazz = Class.forName(className);
+          
         }
+      }
+    } catch (IOException e) {
+      e.printStackTrace(); 
+    } catch (ClassNotFoundException e) {
+      e.printStackTrace();  
     }
 
-    public static void initAccess() {
-        
-    }
+  }
 
-    public static void initPlugin(String jar_path) {
-        JarFile jarFile = null;
-        try {
-            jarFile = new JarFile(jar_path);
-            Enumeration<?> enu = jarFile.entries();
-            while (enu.hasMoreElements()) {
-                JarEntry entry = (JarEntry) enu.nextElement();
-                String name = entry.getName();
-                if (name.equals("plugin.xml")) {
-                    InputStream input = null;
-                    input = jarFile.getInputStream(entry);
-                    Document document = XmlUtils.loadDocument(input);
-                    //取得菜单
-                    Menu menu = new Menu();
-                    List<?> menu_list = XmlUtils.getElementList(document, "/Plugin/Menus/Menu", "@id");
-                    initMenus(menu_list, document, menu);
-                    System.out.println(">>>>>>>>>>>>>>:"+menu.toString());
-                    //取得依赖plugin list
-                    List<String> requires = initRequire(document);
+  public static void initPlugin(String jar_path) {
+    JarFile jarFile = null;
+    try {
+      jarFile = new JarFile(jar_path);
+      Enumeration<?> enu = jarFile.entries();
+      while (enu.hasMoreElements()) {
+        JarEntry entry = (JarEntry) enu.nextElement();
+        String name = entry.getName();
+        if (name.equals("plugin.xml")) {
+          InputStream input = null;
+          input = jarFile.getInputStream(entry);
+          Document document = XmlUtils.loadDocument(input);
+          //取得菜单
+          Menu menu = new Menu();
+          List<?> menu_list = XmlUtils.getElementList(document, "/Plugin/Menus/Menu", "@id");
+          initMenus(menu_list, document, menu);
+          System.out.println(">>>>>>>>>>>>>>:" + menu.toString());
+          //取得依赖 plugin list
+          List<String> requires = initRequire(document);
 
-                    Element element = document.getRootElement();
-                    String clazz_name = element.attributeValue("class");
-                    String plugin_name = element.attributeValue("name");
-                    BasePlugin bp = (BasePlugin) Class.forName(clazz_name).newInstance();
-                    bp.Init();  //初始化DataKind 
-                    bp.setMenus(menu);
-                    plugins.put(plugin_name, bp);
-                }
-            }
-            jarFile.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        } catch (InstantiationException e) {
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        } catch (NexusException e) {
-            e.printStackTrace();
+          Element element = document.getRootElement();
+          String clazz_name = element.attributeValue("class");
+          String plugin_name = element.attributeValue("name");
+          BasePlugin bp = (BasePlugin) Class.forName(clazz_name).newInstance();
+          bp.Init();  //初始化DataKind
+          bp.setMenus(menu);
+          plugins.put(plugin_name, bp);
         }
+      }
+      jarFile.close();
+    } catch (IOException e) {
+      e.printStackTrace();
+    } catch (ClassNotFoundException e) {
+      e.printStackTrace();
+    } catch (InstantiationException e) {
+      e.printStackTrace();
+    } catch (IllegalAccessException e) {
+      e.printStackTrace();
+    } catch (NexusException e) {
+      e.printStackTrace();
     }
+  }
 
-    protected static void initMenus(List<?> list, Document doc, Menu menu) throws NexusException {
+  protected static void initMenus(List<?> list, Document doc, Menu menu) throws NexusException {
 
-        Iterator<?> iterator = list.iterator();
-        while (iterator.hasNext()) {
-            Element element = (Element) iterator.next();
-            if (XmlUtils.elementIsExistChild(element)) {
-                Menu childMenu = new Menu(
-                        element.attributeValue("id"),
-                        element.attributeValue("name"),
-                        "",
-                        true);
-                menu.add(childMenu);
-                List<?> childList = element.elements();
-                initMenus(childList, doc, childMenu);
-            } else {
-                if (!XmlUtils.elementIsExistAttribute(element, "url")) {
-                    continue;
-                }
-                Menu linkMenu = new Menu(
-                        element.attributeValue("id"),
-                        element.attributeValue("name"),
-                        element.attributeValue("url"),
-                        element.attributeValue("roles"),                        
-                        false);
-                menu.add(linkMenu);
-            }
+    Iterator<?> iterator = list.iterator();
+    while (iterator.hasNext()) {
+      Element element = (Element) iterator.next();
+      if (XmlUtils.elementIsExistChild(element)) {
+        Menu childMenu = new Menu(
+            element.attributeValue("id"),
+            element.attributeValue("name"),
+            "",
+            true);
+        menu.add(childMenu);
+        List<?> childList = element.elements();
+        initMenus(childList, doc, childMenu);
+      } else {
+        if (!XmlUtils.elementIsExistAttribute(element, "url")) {
+          continue;
         }
+        Menu linkMenu = new Menu(
+            element.attributeValue("id"),
+            element.attributeValue("name"),
+            element.attributeValue("url"),
+            element.attributeValue("roles"),
+            false);
+        menu.add(linkMenu);
+      }
     }
+  }
 
-    protected static List<String> initRequire(Document doc) {
-        return null;
-    }
+  protected static List<String> initRequire(Document doc) {
+    return null;
+  }
 
-    public static Map<String, BasePlugin> getPlugins() {
-        return plugins;
-    }
+  public static Map<String, BasePlugin> getPlugins() {
+    return plugins;
+  }
 }
