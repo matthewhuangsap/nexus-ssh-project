2555470dc19b481a15ef471a7a5d94fd9caba59d
dcriori <dcriori@58a0b488-0aed-11df-89ef-dd47c58a1d1c>
2010-04-14 04:53:14 +0000

    mark update role

--- trunk/nexus-enterprise/src/main/java/it/nexus/enterprise/system/framework/controllers/LoginController.java
+++ trunk/nexus-enterprise/src/main/java/it/nexus/enterprise/system/framework/controllers/LoginController.java
@@ -1,18 +1,12 @@
 package it.nexus.enterprise.system.framework.controllers;
 
+import com.opensymphony.xwork2.ActionContext;
 import it.nexus.core.annotation.Access;
 import it.nexus.core.controller.BaseAction;
 import it.nexus.core.tools.ClassUtils;
 import it.nexus.enterprise.system.framework.services.SystemAssistService;
 import it.nexus.enterprise.system.user.model.User;
 import it.nexus.enterprise.system.user.service.UserService;
-
-import java.util.List;
-import java.util.Map;
-
-import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
-
 import org.apache.struts2.convention.annotation.Action;
 import org.apache.struts2.convention.annotation.ParentPackage;
 import org.apache.struts2.convention.annotation.Result;
@@ -21,101 +15,99 @@ import org.apache.struts2.interceptor.SessionAware;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 
-import com.opensymphony.xwork2.ActionContext;
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+import java.util.Map;
 
 @SuppressWarnings("unchecked")
 @Controller
 @Scope("prototype")
 @ParentPackage(value = "struts-default")
 public class LoginController extends BaseAction implements ServletRequestAware,
-		SessionAware
-{
-	private static final long	serialVersionUID	= 1788765307656307005L;
-	/**
-	 * Dec 17, 2009 LoingController.java Administrator
-	 */
-	private User				dmo;
+    SessionAware {
+  private static final long serialVersionUID = 1788765307656307005L;
+  /**
+   * Dec 17, 2009 LoingController.java Administrator
+   */
+  private User dmo;
 
-	public User getDmo() {
-		return dmo;
-	}
+  public User getDmo() {
+    return dmo;
+  }
 
-	public void setDmo(User dmo) {
-		this.dmo = dmo;
-	}
+  public void setDmo(User dmo) {
+    this.dmo = dmo;
+  }
 
-	@Resource
-	UserService			userService;
+  @Resource
+  UserService userService;
 
-	@Resource
-	SystemAssistService	systemAssistService;
+  @Resource
+  SystemAssistService systemAssistService;
 
-	@Action(value = "show", results = { @Result(type = "freemarker", location = "/login.ftl", name = "success") })
-	public String show() throws Exception {
-		return super.execute();
-	}
+  @Action(value = "show", results = {@Result(type = "freemarker", location = "/login.ftl", name = "success")})
+  public String show() throws Exception {
+    return super.execute();
+  }
 
-	@Access("访问")
-	@Action(value = "login", results = { @Result(type = "redirect", location = "default", name = "success") })
-	public String login() throws Exception {
-    if((dmo.getUsername().equalsIgnoreCase("admin")
-            && dmo.getPassword().equalsIgnoreCase("admin"))){
+  @Access("访问")
+  @Action(value = "login", results = {@Result(type = "redirect", location = "default", name = "success")})
+  public String login() throws Exception {
+    if ((dmo.getUsername().equalsIgnoreCase("admin")
+        && dmo.getPassword().equalsIgnoreCase("admin"))) {
       getSession().put("userinfo", dmo);
       return super.execute();
-		}
+    }
 
-		if (userService.isLogin(dmo.getUsername(), dmo.getPassword())) {
-			getSession().put("userinfo", dmo);
-			dmo = userService.getUser(dmo.getUsername(), dmo.getPassword());
-			Map<String, Map<String,Long>> roleActions = userService.getRoleActions(dmo);
-			getSession().put("roles", roleActions);
+    if (userService.isLogin(dmo.getUsername(), dmo.getPassword())) {
+      getSession().put("userinfo", dmo);
+      dmo = userService.getUser(dmo.getUsername(), dmo.getPassword());
+      Map<String, Map<String, Long>> roleActions = userService.getRoleActions(dmo);
+      getSession().put("roles", roleActions);
 
-			getSession().put("access_group_map",
-					systemAssistService.getAccessGroupMap());
-			getSession().put("plugin_info",
-					systemAssistService.getPluginInfo());
     } else {
-			ActionContext.getContext().getSession().put("userinfo", null);
-			ActionContext.getContext().getSession().put("roles", null);
-		}
-		getSecurityMap();
-		return super.execute();
-	}
+      ActionContext.getContext().getSession().put("userinfo", null);
+      ActionContext.getContext().getSession().put("roles", null);
+    }
+//    getSecurityMap();
+    return super.execute();
+  }
 
-	private Map<String, Object>	session;
+  private Map<String, Object> session;
 
-	@Override
-	public void setSession(Map<String, Object> sesssion) {
-		this.session = sesssion;
-	}
+  @Override
+  public void setSession(Map<String, Object> sesssion) {
+    this.session = sesssion;
+  }
 
-	private void getSecurityMap() {
-		// TODO 目前这里应该是从各个模块jar中读取配置文件
-		String real_path = request.getSession().getServletContext().getRealPath("/");
-		String config_path = real_path + "//config";
-		List<String> list = ClassUtils.searchFileFromFolder(config_path,".*\\.plugin.xml");
-		for (String string : list) {
-			System.out.println(">>>>>>>" + string);
-		}
-	}
+//  private void getSecurityMap() {
+//    // TODO 目前这里应该是从各个模块jar中读取配置文件
+//    String real_path = request.getSession().getServletContext().getRealPath("/");
+//    String config_path = real_path + "//config";
+//    List<String> list = ClassUtils.searchFileFromFolder(config_path, ".*\\.plugin.xml");
+//    for (String string : list) {
+//      System.out.println(">>>>>>>" + string);
+//    }
+//  }
 
-	private HttpServletRequest	request;
+  private HttpServletRequest request;
 
-	@Override
-	public void setServletRequest(HttpServletRequest request) {
-		this.request = request;
-	}
+  @Override
+  public void setServletRequest(HttpServletRequest request) {
+    this.request = request;
+  }
 
-	public HttpServletRequest getRequest() {
-		return request;
-	}
+  public HttpServletRequest getRequest() {
+    return request;
+  }
 
-	public void setRequest(HttpServletRequest request) {
-		this.request = request;
-	}
+  public void setRequest(HttpServletRequest request) {
+    this.request = request;
+  }
 
-	public Map<String, Object> getSession() {
-		return session;
-	}
+  public Map<String, Object> getSession() {
+    return session;
+  }
 
 }
--- trunk/nexus-enterprise/src/main/java/it/nexus/enterprise/system/user/dao/UserDAO.java
+++ trunk/nexus-enterprise/src/main/java/it/nexus/enterprise/system/user/dao/UserDAO.java
@@ -3,61 +3,61 @@ package it.nexus.enterprise.system.user.dao;
 import it.nexus.core.dao.BaseDAO;
 import it.nexus.enterprise.system.authorize.model.RoleAction;
 import it.nexus.enterprise.system.user.model.User;
+import org.hibernate.Query;
+import org.springframework.stereotype.Repository;
+import org.springframework.util.Assert;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.hibernate.Query;
-import org.springframework.stereotype.Repository;
-import org.springframework.util.Assert;
-
 @Repository
-public class UserDAO extends BaseDAO<User,String> implements IUserDAO<User> {
-    @SuppressWarnings("unchecked")
-	public boolean find(final String username,final String password){
-        User user =  this.getUserByNameAndPwd(username,password);
-        Assert.notNull(user, "用户不存在！");
-        if(user.getPassword().equalsIgnoreCase(password))
-			return true;
-		return false;
-	}
-	
-	@SuppressWarnings("unchecked")
-	public User getUserByNameAndPwd(final String username,final String password){
-		String sql = "FROM User AS u WHERE u.username = ? AND u.password = ?";
-		Query query = getSession().createQuery (sql);
-		query.setString(0, username);
-		query.setString(1, password);
-		
-		List list = query.list ( ) ;
-		if(list.size()>0)
-			return (User)list.get(0);
-		return null;
-	}
+public class UserDAO extends BaseDAO<User, String> implements IUserDAO<User> {
+  @SuppressWarnings("unchecked")
+  public boolean find(final String username, final String password) {
+    User user = this.getUserByNameAndPwd(username, password);
+    Assert.notNull(user, "用户不存在！");
+    if (user.getPassword().equalsIgnoreCase(password))
+      return true;
+    return false;
+  }
 
-    Map<String, Map<String,Long>> ROLE_ACTION = new HashMap<String, Map<String,Long>>();
+  @SuppressWarnings("unchecked")
+  public User getUserByNameAndPwd(final String username, final String password) {
+    String sql = "FROM User AS u WHERE u.username = ? AND u.password = ?";
+    Query query = getSession().createQuery(sql);
+    query.setString(0, username);
+    query.setString(1, password);
 
-	public Map<String, Map<String,Long>> getRoleActions(User user){
+    List list = query.list();
+    if (list.size() > 0)
+      return (User) list.get(0);
+    return null;
+  }
 
-		String hql="from RoleAction where role_id=?" ;
-		if(user.getRole() == null)
-			return new HashMap<String,Map<String,Long>>();
-		Query query = createQuery(hql,user.getRole().getId());
-		List<RoleAction> roles = query.list();
-		for (Object role : roles) {
-			RoleAction ra = (RoleAction)role;
-			Map<String,Long> role_item = getRoleItem(((RoleAction) role).getPlugin());
-            role_item.put(ra.getName(),ra.getRolebits());
-		}
-		return ROLE_ACTION;
-	}
+  Map<String, Map<String, Long>> ROLE_ACTION = new HashMap<String, Map<String, Long>>();
 
-    private Map<String, Long> getRoleItem(String plugin) {
-        if(!ROLE_ACTION.containsKey(plugin)){
-            ROLE_ACTION.put(plugin,new HashMap<String,Long>()) ;
-        }
-        return ROLE_ACTION.get(plugin);
+  public Map<String, Map<String, Long>> getRoleActions(User user) {
+
+    String hql = "from RoleAction where role_id=?";
+    if (user.getRole() == null)
+      return new HashMap<String, Map<String, Long>>();
+    Query query = createQuery(hql, user.getRole().getId());
+    List<RoleAction> roles = query.list();
+    for (Object role : roles) {
+      RoleAction ra = (RoleAction) role;
+      Map<String, Long> role_item = getRoleItem(((RoleAction) role).getPlugin());
+      role_item.put(ra.getName(), ra.getRolebits());
     }
+    return ROLE_ACTION;
+  }
+
+  
+  private Map<String, Long> getRoleItem(String plugin) {
+    if (!ROLE_ACTION.containsKey(plugin)) {
+      ROLE_ACTION.put(plugin, new HashMap<String, Long>());
+    }
+    return ROLE_ACTION.get(plugin);
+  }
 
 }
