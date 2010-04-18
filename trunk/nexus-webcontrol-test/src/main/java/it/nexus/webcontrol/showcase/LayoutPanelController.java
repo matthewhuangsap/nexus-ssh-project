bd5608a00eb22fa78857358f82978ad3ed15e3bb
dcriori <dcriori@58a0b488-0aed-11df-89ef-dd47c58a1d1c>
2010-04-18 15:03:45 +0000

    mark add webcontrol test project !

---
+++ trunk/nexus-webcontrol-test/src/main/java/it/nexus/webcontrol/showcase/LayoutPanelController.java
@@ -0,0 +1,18 @@
+package it.nexus.webcontrol.showcase;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Administrator
+ * Date: 2010-4-18
+ * Time: 14:21:37
+ * To change this template use File | Settings | File Templates.
+ */
+public class LayoutPanelController extends ActionSupport {
+    @Override
+    public String execute() throws Exception {
+        
+        return super.execute(); 
+    }
+}
