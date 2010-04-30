f5f46a59840b37d0c61e0d28997627f878176b58
dcriori <dcriori@58a0b488-0aed-11df-89ef-dd47c58a1d1c>
2010-04-30 09:17:29 +0000

    mark

---
+++ trunk/nexus-enterprise/src/main/java/it/nexus/enterprise/framework/controllers/ContainerController.java
@@ -0,0 +1,32 @@
+package it.nexus.enterprise.framework.controllers;
+
+import it.nexus.enterprise.annotations.Access;
+import it.nexus.enterprise.annotations.AccessGroup;
+
+import org.apache.struts2.convention.annotation.Action;
+import org.apache.struts2.convention.annotation.ParentPackage;
+import org.apache.struts2.convention.annotation.Result;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+
+import plugin.system.PluginClass;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+@Controller
+@Scope("prototype")
+@ParentPackage(value = "coolie-default")
+@AccessGroup(name="首页",pluginClass=PluginClass.class)
+public class ContainerController extends ActionSupport {
+	private static final long serialVersionUID = 6197691600769339053L;
+	/**
+	 * Dec 21, 2009 ContainerController.java Administrator
+	 * @throws Exception 
+	 */
+	
+	@Access("访问")
+	@Action(value = "/container", results = { @Result(type = "freemarker", location = "/container.ftl", name = "success") })
+	public String container() throws Exception{
+		return super.execute();
+	}
+}
---
+++ trunk/nexus-enterprise/src/main/java/it/nexus/enterprise/framework/controllers/TopbannerController.java
@@ -0,0 +1,51 @@
+package it.nexus.enterprise.framework.controllers;
+
+import it.nexus.enterprise.system.user.model.User;
+
+import org.apache.struts2.convention.annotation.Action;
+import org.apache.struts2.convention.annotation.ParentPackage;
+import org.apache.struts2.convention.annotation.Result;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+
+import com.opensymphony.xwork2.ActionContext;
+import com.opensymphony.xwork2.ActionSupport;
+
+@Controller
+@Scope("prototype")
+@ParentPackage(value = "coolie-default")
+public class TopbannerController extends ActionSupport {
+	private static final long serialVersionUID = 2991983523496337582L;
+	/**
+	 * Dec 18, 2009 TopbannerController.java Administrator
+	 * @throws Exception 
+	 */
+	
+	
+	private String username;
+	
+	public String getUsername() {
+		return username;
+	}
+
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	@Action(value = "/header", results = { @Result(type = "freemarker", location = "/header.ftl", name = "success") })
+	public String header() throws Exception{
+		User user =(User)ActionContext.getContext().getSession().get("userinfo");
+		if(user==null)
+			return "login";
+		setUsername(user.getUsername());
+		return super.execute();
+	}
+	
+	
+	@Action(value = "/logout", results = { @Result(type = "freemarker", location = "/login.ftl", name = "success") })
+	public String logout() throws Exception{
+		ActionContext.getContext().getSession().put("userinfo", null);
+		return super.execute();
+	}
+}
